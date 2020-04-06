69becd7d3cb3ba73766390fd784bb686a92d24e7
avanin <Alexey.Vanin@noveogroup.com>
2020-04-06 22:26:51 +0300

    init

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,30 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-console.log('webpack starterkit');
+const sendScriptData = () => {
+  const ticketName = window.document.querySelector(
+    "#ticketForm input#ticketName"
+  );
+
+  const ticketNumber = window.document.querySelector(
+    "#ticketForm input#ticketNumber"
+  );
+
+  const ticketData = {
+    name: ticketName.value,
+    number: ticketNumber.value,
+  };
+
+  let scriptUrl = 'https://script.google.com/macros/s/AKfycbyaVNFqnYjd6it5tlO9-B-_aonhXS8adAbatBbHCw96XDZ_mp8/exec';
+  window.open(scriptUrl + '?name=' + encodeURIComponent(ticketData.name) + '&ticketId=' + encodeURIComponent(ticketData.number));
+  console.log(ticketData);
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const ticketName = window.document.querySelector(
+    "#ticketForm button#sendScript"
+  );
+
+  ticketName.addEventListener("click", (e) => {
+    sendScriptData();
+  });
+});
